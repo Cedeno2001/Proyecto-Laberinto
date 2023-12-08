ac77a36614a2ea9d644fdd44f29d817ca0f8dcec
Victoria Cedeño <genecede2001@gmail.com>
2023-12-08 17:26:39 +0100

    parte_3

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import readchar
 from readchar import key
+import os
 nombre = input("Ingresa tu nombre:")
 print(f"Hola, bienvenido {nombre}. Este es un juego de laberinto")
 
+def borrarterminal():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def main():
-    print("entraste en un bucle, para salir tiene que presionar la tecla UP")
-    while True:
-        teclas = readchar.readkey()
-        print(f"tecla seleccionada:{teclas}")
-        if teclas == key.UP:
-            break
-    print("Felicidades, lograste salir del bucle!!") 
+    numero = 0
+    while numero <= 50:
+        borrarterminal()
+        print(f"haz presionado {numero}:")
+        
+        tecla = readchar.readkey()
+        
+        if tecla == "n":
+            numero += 1
+            if numero == 50:
+                borrarterminal()
+                print("llegaste a 50. Lo lograste!!") 
+                break   
     
     
 if __name__ == "__main__": 
-    main()      
+    main()
+
+   
+      
