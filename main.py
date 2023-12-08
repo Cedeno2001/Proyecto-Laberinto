5ab11c622a7701fa46f13fe44f6fc7865c1aa332
Victoria Cedeño <genecede2001@gmail.com>
2023-12-08 16:58:25 +0100

    parte_2

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import readchar
+from readchar import key
 nombre = input("Ingresa tu nombre:")
 print(f"Hola, bienvenido {nombre}. Este es un juego de laberinto")
+
+def main():
+    print("entraste en un bucle, para salir tiene que presionar la tecla UP")
+    while True:
+        teclas = readchar.readkey()
+        print(f"tecla seleccionada:{teclas}")
+        if teclas == key.UP:
+            break
+    print("Felicidades, lograste salir del bucle!!") 
     
+    
+if __name__ == "__main__": 
+    main()      
