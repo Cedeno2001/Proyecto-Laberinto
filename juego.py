836de6e33746df694d08744811260edb2d911c2c
Victoria Cedeño <genecede2001@gmail.com>
2023-12-12 22:36:19 +0100

    parte_6, añadido reduce, map

--- juego.py
+++ juego.py
@@ -1,6 +1,7 @@
 import readchar
 import os
 import random
+from functools import reduce
 class Juego:
     def __init__(self, path_a_mapas):
         self.path_a_mapas = path_a_mapas
@@ -17,86 +18,57 @@ class Juego:
             return archivos_de_los_mapas.read().strip()
 
     def cadena_de_matrices(self, cadena_de_los_laberintos):
-        filas_del_laberinto = cadena_de_los_laberintos.strip().split("\n")
-        matriz_del_laberinto = [list(fila) for fila in filas_del_laberinto]
-        return matriz_del_laberinto
+        return list(map(list, cadena_de_los_laberintos.strip().split("\n")))
+
 
     def encontrar_posiciones_inicio_y_fin(self, matriz_del_laberinto):
         for i in range(len(matriz_del_laberinto)):
             for u in range(len(matriz_del_laberinto[i])):
                 if matriz_del_laberinto[i][u] == "P":
                     posicion_inicio = (i, u)
-                elif matriz_del_laberinto[i][u] == "E":
+                elif matriz_del_laberinto[i][u] == ".":
                     posicion_final = (i, u)
         return posicion_inicio, posicion_final
     
-
     def inicializador(self):
         while (self.px, self.py) != self.posicion_fin:
             self.terminal()
-
             for fila in self.matriz_del_laberinto:
                 print("".join(fila))
-
-
-
-
             tecla = readchar.readkey()
-
             if tecla == readchar.key.UP:
-
                 nueva_px, nueva_py = self.px - 1, self.py
-
             elif tecla == readchar.key.DOWN:
-
                 nueva_px, nueva_py = self.px + 1, self.py
-
             elif tecla == readchar.key.LEFT:
-
                 nueva_px, nueva_py = self.px, self.py - 1
-
             elif tecla == readchar.key.RIGHT:
-
                 nueva_px, nueva_py = self.px, self.py + 1
-
             else:
-
                 continue
-
-
-
-
             if (
-
                 0 <= nueva_px < len(self.matriz_del_laberinto)
-
                 and 0 <= nueva_py < len(self.matriz_del_laberinto[0])
-
                 and self.matriz_del_laberinto[nueva_px][nueva_py] != "#"
-
             ):
-
                 self.matriz_del_laberinto[self.px][self.py] = "."
-
                 self.px, self.py = nueva_px, nueva_py
-
                 self.matriz_del_laberinto[self.px][self.py] = "P"
-
-
-
-
         self.terminal()
-
         for fila in self.matriz_del_laberinto:
-
             print("".join(fila))
-
         print("Excelente, finalizaste el laberinto!")
-
     def terminal(self):
-
         os.system("cls" if os.name == "nt" else "clear")
 
 class JuegoArchivo(Juego):
     def __init__(self, path_a_mapas):
         super().__init__(path_a_mapas)
+    
+    def file_map_read(self, file):
+        with open(file, "r") as archivos_de_los_mapas:
+           lines = archivos_de_los_mapas.readlines()
+        coordinates = lines[0].strip().split()
+        row_map = reduce(lambda x, y: x + y, lines[1:])
+        return coordinates, row_map.strip()
+            
